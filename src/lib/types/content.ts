cb780d176c3fd67b703339a29a65d6f6944a52b7
ikusteu <ivankust57@gmail.com>
2025-05-07 14:34:24 +0200

    add chatgpt scraping function

--- src/lib/types/content.ts
+++ src/lib/types/content.ts
@@ -8,9 +8,11 @@ export type Message = {
 
 export type Conversation = {
   id: string,
+  title: string,
   created_at: number,
   updated_at: number,
   /** ChatGPT, Claude, etc. */
   source: string
   messages: Message[]
 }
+
